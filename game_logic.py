b4ba10fb623ec7a605e5861fb14e87a753233c6b
APavlikV <156465430+APavlikV@users.noreply.github.com>
2025-04-04 22:32:10 +0300

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -17,11 +17,14 @@ def check_move(control, attack, chosen_defense):
 
 def generate_short_log(step, control, attack, chosen_defense, is_success, partial_success, correct_answer):
     result_emoji = "🟢" if is_success else "🟠" if partial_success else "🔴"
-    return f"<code>⚔️ Атака {step + 1}</code>\n\n" \
-           f"🎯 Контроль: <b>{control}</b>\n" \
-           f"💥 Атака: <b>{attack}</b>\n" \
-           f"Защита и контратака: <b>{chosen_defense}</b>\n" \
-           f"{result_emoji} <b>{'УСПЕХ' if is_success else 'ПОРАЖЕНИЕ'}</b>" + (f" (правильно: {correct_answer})" if not is_success and correct_answer else "")
+    return (
+        f"<code>⚔️ Атака {step + 1}</code>\n\n"
+        f"🎯 Контроль: <b>{control}</b>\n"
+        f"💥 Атака: <b>{attack}</b>\n"
+        f"Защита и контратака: <b>{chosen_defense}</b>\n"
+        f"{result_emoji} <b>{'УСПЕХ' if is_success else 'ПОРАЖЕНИЕ'}</b>"
+        + (f" (правильно: {correct_answer})" if not is_success and correct_answer else "")
+    )
 
 def generate_detailed_log(control, attack, chosen_defense, is_success):
     attacker_control_success = random.choice([True, False])
