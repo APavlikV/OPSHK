5ba43253a5d5e0959cdb7cf84f0f30bb6bcc2e36
APavlikV <156465430+APavlikV@users.noreply.github.com>
2025-04-06 23:22:24 +0300

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -18,7 +18,7 @@ def check_move(control, attack, chosen_defense):
 def generate_short_log(step, control, attack, chosen_defense, is_success, partial_success, correct_answer):
     result_emoji = "🟢" if is_success else "🟠" if partial_success else "🔴"
     return (
-        f"<code>⚔️ Атака {step + 1}</code>\n\n"
+        f"<code>⚔️ Схватка {step + 1}</code>\n\n"
         f"🎯 Контроль: <b>{control}</b>\n"
         f"💥 Атака: <b>{attack}</b>\n"
         f"Защита и контратака: <b>{chosen_defense}</b>\n"
