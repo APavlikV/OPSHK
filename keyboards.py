eb07a907f55bfb78a927f8ebc0964ce749a22dd1
APavlikV <156465430+APavlikV@users.noreply.github.com>
2025-04-06 14:12:34 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,29 +1,50 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-def start_keyboard():
+
+def get_start_keyboard() -> InlineKeyboardMarkup:
     keyboard = [
         [InlineKeyboardButton("Игра", callback_data="game")],
     ]
     return InlineKeyboardMarkup(keyboard)
 
-def menu_keyboard():
+
+def get_menu_keyboard() -> InlineKeyboardMarkup:
     keyboard = [
-        [InlineKeyboardButton("Правила", callback_data="rules"), InlineKeyboardButton("Памятка", callback_data="memo")],
-        [InlineKeyboardButton("Тренировка", callback_data="training_fight"), InlineKeyboardButton("Арена", callback_data="karate_arena")],
+        [
+            InlineKeyboardButton("Правила", callback_data="rules"),
+            InlineKeyboardButton("Памятка", callback_data="memo")
+        ],
+        [
+            InlineKeyboardButton("Тренировка", callback_data="training_fight"),
+            InlineKeyboardButton("Арена", callback_data="karate_arena")
+        ],
     ]
     return InlineKeyboardMarkup(keyboard)
 
-def training_mode_keyboard():
+
+def get_training_mode_keyboard() -> InlineKeyboardMarkup:
     keyboard = [
-        [InlineKeyboardButton("Простой бой", callback_data="simple_fight"), InlineKeyboardButton("Бой на время", callback_data="timed_fight")],
+        [
+            InlineKeyboardButton("Простой бой", callback_data="simple_fight"),
+            InlineKeyboardButton("Бой на время", callback_data="timed_fight")
+        ],
     ]
     return InlineKeyboardMarkup(keyboard)
 
-def answer_keyboard(send_hint=False):
+
+def get_answer_keyboard(send_hint: bool = False) -> InlineKeyboardMarkup:
     keyboard = [
-        [InlineKeyboardButton("Аге уке", callback_data="Аге уке"), InlineKeyboardButton("Сото уке", callback_data="Сото уке")],
-        [InlineKeyboardButton("Учи уке", callback_data="Учи уке"), InlineKeyboardButton("Гедан барай", callback_data="Гедан барай")],
+        [
+            InlineKeyboardButton("Аге уке", callback_data="Аге уке"),
+            InlineKeyboardButton("Сото уке", callback_data="Сото уке")
+        ],
+        [
+            InlineKeyboardButton("Учи уке", callback_data="Учи уке"),
+            InlineKeyboardButton("Гедан барай", callback_data="Гедан барай")
+        ],
     ]
     if send_hint:
-        keyboard.append([InlineKeyboardButton("💡 Подсказка", callback_data="hint")])
+        keyboard.append([
+            InlineKeyboardButton("💡 Подсказка", callback_data="hint")
+        ])
     return InlineKeyboardMarkup(keyboard)
