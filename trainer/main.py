2c3c2f2f1405f6dbb6274d5c9ae7dbfdecd7b540
APavlikV <156465430+APavlikV@users.noreply.github.com>
2025-04-11 19:29:18 +0300

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -5,7 +5,7 @@ from telegram.ext import (
     Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 )
 from telegram.request import HTTPXRequest
-from trainer.handlers import start, game, button
+from handlers import start, game, button
 
 # --- Глобальный логгер ---
 logging.basicConfig(
