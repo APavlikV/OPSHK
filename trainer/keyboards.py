538e98a65e1aa86ea4d33ac8ef6f273c85029a3c
APavlikV <156465430+APavlikV@users.noreply.github.com>
2025-04-12 16:47:50 +0300

    Update keyboards.py

--- trainer/keyboards.py
+++ trainer/keyboards.py
@@ -1,46 +1,62 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
 def menu_keyboard():
     keyboard = [
-        [InlineKeyboardButton("Тренировать защиту", callback_data="training_fight")],
-        [InlineKeyboardButton("Спортивный поединок", callback_data="pvp_bot")]
+        [InlineKeyboardButton("Тренировка защиты", callback_data="training_fight")],
+        [InlineKeyboardButton("Спортивный бой", callback_data="pvp_bot")]
     ]
     return InlineKeyboardMarkup(keyboard)
 
-def training_mode_keyboard():
+def training_fight_keyboard():
     keyboard = [
-        [InlineKeyboardButton("Без времени", callback_data="simple_fight")],
-        [InlineKeyboardButton("На время", callback_data="timed_fight")]
+        [InlineKeyboardButton("Правила", callback_data="training_rules"),
+         InlineKeyboardButton("Памятка", callback_data="training_memo")],
+        [InlineKeyboardButton("Простой бой", callback_data="simple_fight"),
+         InlineKeyboardButton("Бой на время", callback_data="timed_fight")]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+def training_rules_keyboard():
+    keyboard = [
+        [InlineKeyboardButton("Памятка", callback_data="training_memo")],
+        [InlineKeyboardButton("Простой бой", callback_data="simple_fight"),
+         InlineKeyboardButton("Бой на время", callback_data="timed_fight")]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+def training_memo_keyboard():
+    keyboard = [
+        [InlineKeyboardButton("Правила", callback_data="training_rules")],
+        [InlineKeyboardButton("Простой бой", callback_data="simple_fight"),
+         InlineKeyboardButton("Бой на время", callback_data="timed_fight")]
     ]
     return InlineKeyboardMarkup(keyboard)
 
 def answer_keyboard(send_hint=False):
     keyboard = [
-        [InlineKeyboardButton("Аге уке", callback_data="Аге уке"),
-         InlineKeyboardButton("Сото уке", callback_data="Сото уке")],
-        [InlineKeyboardButton("Учи уке", callback_data="Учи уке"),
-         InlineKeyboardButton("Гедан барай", callback_data="Гедан барай")]
+        [InlineKeyboardButton("Аге уке", callback_data="Аге уке"), InlineKeyboardButton("Сото уке", callback_data="Сото уке")],
+        [InlineKeyboardButton("Учи уке", callback_data="Учи уке"), InlineKeyboardButton("Гедан барай", callback_data="Гедан барай")]
     ]
     if send_hint:
-        keyboard.append([InlineKeyboardButton("💡Подсказка", callback_data="hint")])
+        keyboard.append([InlineKeyboardButton("Подсказка", callback_data="hint")])
     return InlineKeyboardMarkup(keyboard)
 
 def pvp_bot_keyboard():
     keyboard = [
-        [InlineKeyboardButton("Правила", callback_data="pvp_rules")],
-        [InlineKeyboardButton("Начать бой", callback_data="pvp_start")]
+        [InlineKeyboardButton("Начать бой", callback_data="pvp_start")],
+        [InlineKeyboardButton("Правила", callback_data="pvp_rules")]
     ]
     return InlineKeyboardMarkup(keyboard)
 
-def pvp_attack_keyboard(mode="control"):
-    prefix = "attack_control_" if mode == "control" else "attack_hit_"
+def pvp_attack_keyboard(mode):
     keyboard = [
-        [InlineKeyboardButton("СС", callback_data=f"{prefix}СС"),
-         InlineKeyboardButton("ТР", callback_data=f"{prefix}ТР"),
-         InlineKeyboardButton("ДЗ", callback_data=f"{prefix}ДЗ")]
+        [InlineKeyboardButton("СС", callback_data=f"attack_{mode}_СС"), InlineKeyboardButton("ТР", callback_data=f"attack_{mode}_ТР")],
+        [InlineKeyboardButton("ДЗ", callback_data=f"attack_{mode}_ДЗ")]
     ]
     return InlineKeyboardMarkup(keyboard)
 
 def pvp_move_keyboard():
-    keyboard = [[InlineKeyboardButton("Подтвердить", callback_data="pvp_move")]]
+    keyboard = [
+        [InlineKeyboardButton("Подтвердить ход", callback_data="pvp_move")]
+    ]
     return InlineKeyboardMarkup(keyboard)
