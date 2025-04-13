37b7c29f9c3cee5c153f393079a6d6c429f15ec8
APavlikV <156465430+APavlikV@users.noreply.github.com>
2025-04-13 07:30:02 +0300

    Update keyboards.py

--- trainer/keyboards.py
+++ trainer/keyboards.py
@@ -1,52 +1,5 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-def menu_keyboard():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("🥊 Тренировочный бой", callback_data="training_fight")],
-        [InlineKeyboardButton("⚔️ Спортивный бой", callback_data="pvp_bot")],
-        [InlineKeyboardButton("📖 Правила", callback_data="training_rules")],
-        [InlineKeyboardButton("📝 Памятка", callback_data="training_memo")]
-    ])
-
-def training_fight_keyboard():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("⏳ Бой на время", callback_data="timed_fight")],
-        [InlineKeyboardButton("📌 Простой бой", callback_data="simple_fight")],
-        [InlineKeyboardButton("🔙 Назад", callback_data="game")]
-    ])
-
-def training_rules_keyboard():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("🔙 Назад", callback_data="game")]
-    ])
-
-def training_memo_keyboard():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("🔙 Назад", callback_data="game")]
-    ])
-
-def answer_keyboard(send_hint=False):
-    keyboard = [
-        [
-            InlineKeyboardButton("Аге уке", callback_data="Аге уке"),
-            InlineKeyboardButton("Сото уке", callback_data="Сото уке")
-        ],
-        [
-            InlineKeyboardButton("Учи уке", callback_data="Учи уке"),
-            InlineKeyboardButton("Гедан барай", callback_data="Гедан барай")
-        ]
-    ]
-    if send_hint:
-        keyboard.append([InlineKeyboardButton("💡 Подсказка", callback_data="hint")])
-    return InlineKeyboardMarkup(keyboard)
-
-def pvp_bot_keyboard():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("⚔️ Начать бой", callback_data="pvp_start")],
-        [InlineKeyboardButton("📖 Правила", callback_data="pvp_rules")],
-        [InlineKeyboardButton("🔙 Назад", callback_data="game")]
-    ])
-
 def pvp_attack_keyboard(step, control=None):
     if step == "control":
         return InlineKeyboardMarkup([
@@ -57,6 +10,7 @@ def pvp_attack_keyboard(step, control=None):
             ]
         ])
     elif step == "attack":
+        moves = ["СС", "ТР", "ДЗ"]  # Дефолт
         if control == "ДЗ":
             moves = ["СС", "ТР", "ГДН"]
         elif control == "ТР":
@@ -66,8 +20,13 @@ def pvp_attack_keyboard(step, control=None):
         return InlineKeyboardMarkup([
             [InlineKeyboardButton(move, callback_data=f"attack_hit_{move}") for move in moves]
         ])
+    return InlineKeyboardMarkup([])
 
-def pvp_move_keyboard():
+def end_fight_keyboard():
     return InlineKeyboardMarkup([
-        [InlineKeyboardButton("✅ Подтвердить ход", callback_data="pvp_move")]
+        [
+            InlineKeyboardButton("Продолжить тренировку", callback_data="simple_fight"),
+            InlineKeyboardButton("Бой на время", callback_data="timed_fight"),
+        ],
+        [InlineKeyboardButton("Прошлая статистика", callback_data="last_stats")]
     ])
